5a558e288c973fc402f30233c588388e43021245
lonewolfheaven46-jpg <lonewolfheaven46@gmail.com>
2025-08-31 16:32:49 -0400

    Guessing game  🎮.py

--- Guessing game  🎮.py
+++ Guessing game  🎮.py
@@ -1,10 +1,3 @@
-echo "# Number-games" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/lonewolfheaven46-jpg/Number-games.git
-git push -u origin main
 
 import random
 
